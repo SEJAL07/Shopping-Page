5c07fe5ca6aeeb2557be148266a42817a48b1c0c
sejal choudhary <sejalc.07@gmail.com>
2018-01-13 02:20:28 +0530

    Project setup completed succesfully again

---
+++ jewelWorld/src/main/java/com/web/jewelWorld/controller/pageController.java
@@ -0,0 +1,37 @@
+package com.web.jewelWorld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "home");
+		mv.addObject("userClickHome", true);
+		
+		//passing category list
+		
+		//mv.addObject("categories",categoryDAO.list());
+		
+		return mv;
+	}	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+} 
+	
+}
